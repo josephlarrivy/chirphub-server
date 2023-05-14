329281ffc2126754800127d3c42a5f188542037b
josephlarrivy <joseph.larrivy@gmail.com>
2023-05-14 09:46:59 -0500

    x

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_debugtoolbar import DebugToolbarExtension
 from models import connect_db, db, User, Chirp, Tag, ChirpTag, Comment, Like, Bookmark
-from itsdangerous import TimedJSONWebSignatureSerializer
+# from itsdangerous import TimedJSONWebSignatureSerializer
 from flask_sqlalchemy import SQLAlchemy
 
 
